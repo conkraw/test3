22a4e54f208e759d83779bb371cbb0bd6814d540
conkraw <conkraw@gmail.com>
2024-10-04 16:13:31 -0400

    Update app.py

--- app.py
+++ app.py
@@ -74,8 +74,10 @@ def main():
         st.session_state.document_id = login_page(users, db) 
     elif st.session_state.page == "intake_form":
         display_intake_form(db, st.session_state.document_id)
+        save_user_state(db) 
     elif st.session_state.page == "diagnoses":
         display_diagnoses(db,st.session_state.document_id)
+        save_user_state(db) 
     elif st.session_state.page == "Intervention Entry":
         intervention_entry_main(db,st.session_state.document_id)
     elif st.session_state.page == "History with AI":
