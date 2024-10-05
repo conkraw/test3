8986446f1ba3c216c25145726bd56a4410c3b816
conkraw <conkraw@gmail.com>
2024-10-05 15:00:26 -0400

    Update login.py

--- utils/login.py
+++ utils/login.py
@@ -1,6 +1,4 @@
 import streamlit as st
-from utils.session_management import collect_session_data
-from utils.firebase_operations import upload_to_firebase
 
 def login_page(users, db):
     st.markdown("<p style='font-family: \"DejaVu Sans\";'>Please enter your unique code to access the assessment.</p>", unsafe_allow_html=True)
@@ -13,25 +11,16 @@ def login_page(users, db):
             
             # Check if the unique_code exists in the 'code' column of the users dataframe
             if unique_code in users['code'].values:
-                # Store the unique code in session state
-                st.session_state.unique_code = unique_code
-
-                # Define the entry data to be uploaded to Firebase (optional)
-                entry = {
-                    "unique_code": unique_code,
-                    # Add any other session data as needed
-                }
-
-                # Upload the session data to Firebase (if necessary)
-                # upload_message = upload_to_firebase(db, st.session_state.document_id, entry)
+                # Store the unique code and user name in session state
+                st.session_state.user_name = users.loc[users['code'] == unique_code, 'name'].values[0]
+                st.session_state.user_code = unique_code
 
                 # Navigate to the intake form page
                 st.session_state.page = "intake_form"  # Change to assessment page
-                st.session_state.document_id = unique_code  # Set document ID for future uploads
+                st.session_state.document_id = unique_code
                 st.rerun()  # Rerun to refresh the view
             else:
                 st.error("Invalid code. Please try again.")
         else:
             st.error("Please enter a code.")
-
 
