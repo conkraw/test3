fd180ba86830d8bde938a9316fb1c1d8ab177662
conkraw <conkraw@gmail.com>
2024-10-05 15:05:06 -0400

    Update login.py

--- utils/login.py
+++ utils/login.py
@@ -18,7 +18,7 @@ def login_page(users, db):
                 # Navigate to the intake form page
                 st.session_state.page = "intake_form"  # Change to assessment page
                 st.session_state.document_id = unique_code
-                #st.rerun()  # Rerun to refresh the view
+                st.rerun()  # Rerun to refresh the view
             else:
                 st.error("Invalid code. Please try again.")
         else:
