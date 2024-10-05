d469515613abdb7123e2c9885efca464456bc65b
conkraw <conkraw@gmail.com>
2024-10-05 14:57:49 -0400

    Update app.py

--- app.py
+++ app.py
@@ -25,21 +25,13 @@ from utils.firebase_operations import initialize_firebase, upload_to_firebase
 from utils.session_management import collect_session_data
 import uuid  # To generate unique document IDs
 
-def load_user_data(db):
-    collection_name = st.secrets["FIREBASE_COLLECTION_NAME"]
-    if st.session_state.user_code:
-        user_data = db.collection(collection_name).document(st.session_state.user_code).get()
-        if user_data.exists:
-            return user_data.to_dict()
-    return {}
-        
 def main():
     # Initialize Firebase
     db = initialize_firebase()
     
     # Initialize session state
-    if "user_code" not in st.session_state: ###
-        st.session_state.user_code = None ###
+    if "unique_code" not in st.session_state:
+        st.session_state.unique_code = None
         
     if "page" not in st.session_state:
         st.session_state.page = "welcome"
@@ -48,48 +40,43 @@ def main():
     if "document_id" not in st.session_state:
         st.session_state.document_id = None    
 
-    if st.session_state.user_code:
-        last_page = load_last_page(db)
-        if last_page:
-            st.session_state.page = last_page
-
     # Page routing
     if st.session_state.page == "welcome":
         welcome_page()
     elif st.session_state.page == "login":
         users = load_users()
-        #login_page(users)  # Pass document ID
         st.session_state.document_id = login_page(users, db) 
     elif st.session_state.page == "intake_form":
         display_intake_form(db, st.session_state.document_id)
     elif st.session_state.page == "diagnoses":
-        display_diagnoses(db,st.session_state.document_id)
+        display_diagnoses(db, st.session_state.document_id)
     elif st.session_state.page == "Intervention Entry":
-        intervention_entry_main(db,st.session_state.document_id)
+        intervention_entry_main(db, st.session_state.document_id)
     elif st.session_state.page == "History with AI":
-        run_virtual_patient(db,st.session_state.document_id)
+        run_virtual_patient(db, st.session_state.document_id)
     elif st.session_state.page == "Focused Physical Examination":
-        display_focused_physical_examination(db, st.session_state.document_id)  # Pass document ID
+        display_focused_physical_examination(db, st.session_state.document_id)
     elif st.session_state.page == "Physical Examination Components":
         display_physical_examination()
     elif st.session_state.page == "History Illness Script":
-        history_illness_script(db,st.session_state.document_id)
+        history_illness_script(db, st.session_state.document_id)
     elif st.session_state.page == "Physical Examination Features":
-        display_physical_examination_features(db,st.session_state.document_id)
+        display_physical_examination_features(db, st.session_state.document_id)
     elif st.session_state.page == "Laboratory Tests":
-        display_laboratory_tests(db,st.session_state.document_id)
+        display_laboratory_tests(db, st.session_state.document_id)
     elif st.session_state.page == "Radiology Tests":
-        display_radiological_tests(db,st.session_state.document_id)
+        display_radiological_tests(db, st.session_state.document_id)
     elif st.session_state.page == "Other Tests":
-        display_other_tests(db,st.session_state.document_id)
+        display_other_tests(db, st.session_state.document_id)
     elif st.session_state.page == "Results":
         display_results_image()
     elif st.session_state.page == "Laboratory Features":
-        display_laboratory_features(db,st.session_state.document_id)
+        display_laboratory_features(db, st.session_state.document_id)
     elif st.session_state.page == "Treatments":
-        display_treatments(db,st.session_state.document_id)
+        display_treatments(db, st.session_state.document_id)
     elif st.session_state.page == "Simple Success":
         display_simple_success1()
 
 if __name__ == "__main__":
     main()
+
